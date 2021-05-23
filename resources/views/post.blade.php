6bd2ed396408ae38cb499545fe6c7c3178008cc4
DeLayne LaBove <delaynelabove@DeLaynes-MacBook-Pro.local>
2021-05-23 13:09:44 -0500

    reformatted code to dynamically display posts and redirect to 404 if post is not found

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,13 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Dev Diaries</title>
+    <link rel="stylesheet" href="/app.css">
 </head>
 <body>
+
 <article>
-<h1></h1>
-    <a href="/">Go Back</a>
+
+ <?= $post; ?>
+
 </article>
+<a href="/">Go Back</a>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
-Route::get('/post', function () {
+
+Route::get('posts/{post}', function ($slug) {
+$path= __DIR__ . "/../resources/posts/$slug.html";
+
+if (! file_exists($path)) {
+
+   abort(404);
+}
+    $post = file_get_contents($path);
+
     return view('post', [
-        'post' => file_get_contents(__DIR__ . '/../resources/posts'
-        )
+        'post' => $post
     ]);
+
+
 });
