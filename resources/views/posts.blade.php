5cef595b6661b244305df0f54c13de20def5e9b5
DeLayne LaBove <delaynelabove@DeLaynes-MacBook-Pro.local>
2021-05-23 13:55:33 -0500

    reformatting

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,21 +11,22 @@
 <body>
 <article>
 
-    <h2><a href="/post">Laravel + PHP: The Beginning</a></h2>
+    <h2><a href="/posts/the-beginning">Laravel + PHP: The Beginning</a></h2>
     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     </p>
 </article>
 
 <article>
-    <h2><a href="/post">Interviewing: Imposter Sydrome</a></h2>
+    <h2><a href="/posts/imposter-syndrome">Interviewing: Imposter Sydrome</a></h2>
     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     </p>
 </article>
 <article>
-    <h2><a href="/post">A Day in the Life: Work Ethics</a></h2>
+    <h2><a href="/posts/work-ethics">A Day in the Life: Work Ethics</a></h2>
     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     </p>
 </article>
+
 
 
 </body>
