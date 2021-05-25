95c167ce1da34d81e21f4390f96b0d5a303298af
DeLayne LaBove <delayne.labove@gmail.com>
2021-05-24 20:09:33 -0500

    added meta data

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,31 @@
 <?php
-//
-//use App\Models\Post;
-//use Illuminate\Support\Facades\Route;
-//
-///*
-//|--------------------------------------------------------------------------
-//| Web Routes
-//|--------------------------------------------------------------------------
-//|
-//| Here is where you can register web routes for your application. These
-//| routes are loaded by the RouteServiceProvider within a group which
-//| contains the "web" middleware group. Now create something great!
-//|
-//*/
-//
-//Route::get('/', function () {
-//
-////    return view('posts', [
-////        'posts' => Post::all()
-////    ]);
-//});
-//
-//Route::get('posts/{post}', function ($slug) {
-//    return view('post', [
-//        'post' =>  $post = Post::find($slug)
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+   $document = \Spatie\YamlFrontMatter\YamlFrontMatter::parseFile(
+        resource_path('posts/work-ethics.html')
+    );
+
+    //    return view('posts', [
+//        'posts' => Post::all()
 //    ]);
-//})->where('post', '[A-z_\-]+');
+});
+
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post' =>  $post = Post::find($slug)
+    ]);
+})->where('post', '[A-z_\-]+');
